3e9b3ba1c417db8c2eff77940416f0b12b959f51
lalix <levriera@uia.no>
2024-03-05 10:21:48 +0100

    remove node modules

--- front-end/src/app/scripts/welcome.js
+++ front-end/src/app/scripts/welcome.js
@@ -40,7 +40,6 @@
 
   // TODO #class: turn function into a method of WelcomeComponent
   function _startGame(name, size) {
-    // TODO #spa: replace with './#game'
     var gamePage = './#game';
     // TODO #template-literals:  use template literals (backquotes)
     window.location = gamePage + "?name=" + name + "&size=" + size;
